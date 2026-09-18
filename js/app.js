55b8f712f5908c1adbd6e7b3a36133a696458cbc
jacobfenn <j.fenn@tcu.edu>
2026-09-18 11:33:15 -0500

    Lab12Work
    12

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-console.log('app.js connected');
+const myFavorite = {
+  name: 'Starbucks on University Drive',
+  category: 'coffee',
+  rating: 5,
+  notes: 'Great study spot with fast WiFi',
+  dateAdded: 'May 2026'
+};
+
+console.log(myFavorite);
+console.log(myFavorite.name);
+console.log(myFavorite.rating);
+
+
+
+const displayText = myFavorite.name + ' - Rating: ' + myFavorite.rating + '/5';
+console.log(displayText);
+
+
+
+console.log(typeof myFavorite.name);
+console.log(typeof myFavorite.category);
+console.log(typeof myFavorite.rating);
+console.log(typeof myFavorite.notes);
+console.log(typeof myFavorite.dateAdded);
+
+
+const placeName = 'Starbucks';
+const rating = 5;
+console.log(placeName + ' - ' + rating + '/5');
+console.log('⭐'.repeat(rating) + ' ' + placeName);
